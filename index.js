70b1a26d0d9603814d1da7f9865f059c7c686082
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-01 18:28:03 +0100

    Aggiunta menu registrati

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // Array utenti registrati
 const utenti = [["lorenzomonteforte@gmail.com", "lormon123"], ["alessiomonteforte@gmail.com", "alemon123"], ["andreamonteforte@gmail.com", "andmon123"]];
+let registrati = document.getElementById("registrati");
+registrati.addEventListener("click", function(){
+    let emailReg = document.getElementById("emailReg").value;
+    let emailRegCon = document.getElementById("emailRegCon").value;
+    let passwordReg = document.getElementById("passwordReg").value;
+    let passwordRegCon = document.getElementById("passwordRegCon").value;
+    if(!(emailReg=="" || emailRegCon=="" || passwordReg=="" || passwordRegCon=="")){
+        if(emailReg==emailRegCon && passwordReg==passwordRegCon){
+            let puoiRegistrarti = true;
+            for(let i=0; i<utenti.length; i++){
+                if(emailReg==utenti[i][0]){
+                    puoiRegistrarti = false;
+                    console.log("Errore. Email già in uso");
+                }
+            }
+            if(puoiRegistrarti==true){
+                utenti.push([emailReg, passwordReg]);
+                console.log("Registrazione avvenuta con successo");
+            }
+        }else{
+            if(emailReg!=emailRegCon && passwordReg!=passwordRegCon){
+                console.log("Errore. Sia le email che le password non coincidono");
+            }else if(emailReg!=emailRegCon){
+                console.log("Errore. Le email non coincidono");
+            }else if(passwordReg!=passwordRegCon){
+                console.log("Errore. Le password non coincidono");
+            }
+        }
+    }else{
+        console.log("Errore. Assicurati di aver compilato tutti i campi");
+    }
+})
 // Click bottone accedi
 const accedi = document.getElementById("accedi");
 accedi.addEventListener("click", function(){
